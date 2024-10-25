153bf2c1f07b096fd40a11357501182ec9b415f8
Harry Developer <harry.developer001@gmail.com>
2024-10-25 13:41:51 +0530

    the wild oasis - styled component, supabase

---
+++ More Exercises/the-wild-oasis/src/pages/Cabins.jsx
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+import Heading from "../ui/Heading";
+import Row from "../ui/Row";
+import { getCabins } from "../services/apiCabins";
+
+function Cabins() {
+  useEffect(() => {
+    async function fetchCabins() {
+      const cabins = await getCabins();
+      console.log(JSON.stringify(cabins));
+    }
+
+    fetchCabins();
+  }, []);
+
+  return (
+    <Row type="horizontal">
+      <Heading as="h1">All cabins</Heading>
+      <p>TEST</p>
+    </Row>
+  );
+}
+
+export default Cabins;
