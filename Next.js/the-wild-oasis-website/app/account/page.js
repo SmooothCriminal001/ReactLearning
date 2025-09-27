b26c22bd5b757ef0b87b5a266fea9e5c9461fdb4
Harry Developer <harry.developer001@gmail.com>
2025-09-27 17:53:18 +0530

    Next js - wild oasis - more additions

--- Next.js/the-wild-oasis-website/app/account/page.js
+++ Next.js/the-wild-oasis-website/app/account/page.js
@@ -1,3 +1,10 @@
+export const metadata = {
+  title: "Your Account",
+};
 export default function Page() {
-  return <h1>Your Account</h1>;
+  return (
+    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+      Welcome, Hari
+    </h2>
+  );
 }
