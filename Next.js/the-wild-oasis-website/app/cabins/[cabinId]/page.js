14fa763cbd8a2d70a259ed4aa2c669bae37ae251
Harry Developer <harry.developer001@gmail.com>
2025-10-17 19:05:06 +0530

    Updates

--- Next.js/the-wild-oasis-website/app/cabins/[cabinId]/page.js
+++ Next.js/the-wild-oasis-website/app/cabins/[cabinId]/page.js
@@ -1,4 +1,4 @@
-import { getCabin } from "@/app/_lib/data-service";
+import { getCabin, getCabins } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 
@@ -19,6 +19,13 @@ export async function generateMetadata({ params }) {
   return {
     title: `Cabin ${cabin.name}`,
   };
+}
+
+export async function generateStaticParams() {
+  const cabins = await getCabins();
+  return cabins.map((cabin) => ({
+    cabinId: String(cabin.id),
+  }));
 }
 
 export default async function Page({ params }) {
